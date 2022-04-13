c6890b2179e7c0360b70492e648a906b0651a49e
WhileCrocodile <98533784+WhileCrocodile@users.noreply.github.com>
2022-04-13 00:27:43 +0000

    forgot to commit; no changes made past april 4

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#### CS 110
+![image](image.png)#### CS 110
 # Midterm - Python Programming
 
 ***
@@ -7,21 +7,35 @@ _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
 What did you clean up?:
-* Collected some functions under a Dartboard class
-* Functions (methods) under the Dartboard class now uses the arguments passed to and initialized by the Dartboard class (e.g. circle radius), rather than individually hardcoded numbers
-* The Dartboard class creates its own window and turtle, which is reused for its functions
+* Collected some functions under a Dartboard class.
+* Functions (methods) under the Dartboard class now uses the arguments passed to and initialized by the Dartboard class (e.g. circle radius), rather than individually hardcoded numbers.
+* The Dartboard class creates its own window and turtle, which is reused for its functions (meaning the window and turtle do not have to be passed every time these functions/methods are called)
+* In drawSquare(), replaced myturtle.goto() with drawLine(), and repetitive calculations in the arguments with two local variables.
+* Replaced two calls to drawLine() in setUpDartboard() with one call to drawAxes()
 
 Summary of function(s) added:
-* The setCircle() method of the Dartboard class allows the user to set another radius for the circle.
+* The setCircle(circle_radius) method of the Dartboard class allows the user to set another radius for the circle.
+* The clearBoard() method clears the drawings of the Turtle object created by the Dartboard object.
+* The guessDart(guess) takes a guess of "hit" (landing in the circle) or "miss" (landing outside the circle) as an argument. If the guess is correct, it returns True. If it is incorrect, it returns False. Note that this is not very fun, as you are always more likely to land in the circle than outside it.
+* The dartRandom() function allows you to throw a number of darts, then tells you how many hit. Not actually random, since darts are more likely to hit the dartboard than miss it. It's random in dart physics.
+* The drawAxes() function draws symmetric x and y axes based on location and size.
+* The hitBounds() function determines if a turtle object hits a boundary, and outputs which boundary it hit (directional wall, or a corner).
+* The screenSaver() functions takes a turtle and bounces symmetrically around a defined area, and traces a line where it goes.
 
 Summary of Feature Added:
+* Added a screensaver through the screenSaver() function.
+* The screenSaver() function uses the turtle belonging to the Dartboard object (at least the way it's called in main() ) and bounces it around a defined area.
+* The screenSaver() function can also take other turtles and bounce it around custom-defined areas.
+* If you leave the screenSaver() running for long enough, it makes a pretty satisfying pattern.
 
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+N/A
 
 ## REFERENCES:
- < List any outside resources used >
+* turtle documentation
+* time documentation
+* random documentation
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+* The hitBounds() and screenSaver() function became more complicated because I had to account for not just hitting the walls, but hitting the corners. Hopefully the code is still readable.
--- main.py
+++ main.py
@@ -26,39 +26,216 @@ Functions you must implement:
 '''
 import turtle
 import random
+import time
 
 #########################################################
 #                   Your Code Goes Below                #
 #########################################################
+def drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
+  '''
+  Draws a square.
+  
+  args:
+    myturtle: (turtle.Turtle) a turtle object
+    width: (int) width of the square
+    top_left_x: (int) x coordinate of top left corner
+    top_right_y: (int) y coordinate of top left corner
+  '''
+  top_right_x = top_left_x+width
+  bottom_right_y = top_left_y-width
+  drawLine(myturtle, top_left_x, top_left_y, top_right_x, top_left_y)
+  drawLine(myturtle, top_right_x, top_left_y, top_right_x, bottom_right_y)
+  drawLine(myturtle, top_right_x, bottom_right_y, top_left_x, bottom_right_y)
+  drawLine(myturtle, top_left_x, bottom_right_y, top_left_x, top_left_y)
+  
+
+def drawLine(myturtle=None, x_start=0, y_start=0, x_end=0, y_end=0):
+  '''
+  Draws a line from point A to point B.
+  
+  args:
+    myturtle: (turtle.Turtle) a turtle object
+    x_start: (int) x coordinate of starting point
+    y_start: (int) y coordinate of starting point
+    x_end: (int) x coordinate of ending point
+    y_end: (int) y coordinate of ending point
+  '''
+  myturtle.penup()
+  myturtle.goto(x_start, y_start)
+  myturtle.pendown()
+  myturtle.goto(x_end, y_end)
+  
+
+def drawCircle(myturtle=None, radius=0, resolution = 100):
+  '''
+  Draws a circle from the bottom up.
+  
+  args:
+    myturtle: (turtle.Turtle) a turtle object
+    radius: (int) radius of the drawn circle
+    resolution: (int) how many lines the turtle uses to draw the circle
+  '''
+  myturtle.pendown()
+  myturtle.circle(radius, steps=resolution)
+
+
+def drawAxes(myturtle=None, axis_width=1, origin_x=0, origin_y=0):
+  '''
+  Draws x and y axes centered at a defined origin.
+  
+  args:
+    myturtle: (turtle.Turtle) a turtle object
+    axis_width: (int) length of one end of an axis to the other end
+    origin_x: (int) x coordinate of the origin
+    origin_y: (int) y coordinate of the origin
+  '''
+  upper_xy = axis_width/2
+  lower_xy = -axis_width/2
+  drawLine(myturtle, x_start=lower_xy, y_start=origin_y, x_end=upper_xy, y_end=origin_y)
+  drawLine(myturtle, x_start=origin_x, y_start=lower_xy, x_end=origin_x, y_end=upper_xy)
+
+
+def hitBounds(myturtle=None, wn_height=10, wn_width=10):
+  '''
+  Determines if a turtle hits the boundary of a set area, and 
+  returns a hit in the format "hit_{bottom/top}{left/right}_bound",
+  e.g. "hit_bottom_bound" or "hit_topright_bound"
+  
+  args:
+    myturtle: (turtle.Turtle) a turtle object
+    wn_height: (int) height of bound area
+    wn_width: (int) width of bound area
+
+  return:
+    False: (bool) turtle did not hit a bound
+    f"hit_{vertical}{horizontal}_bound": (str) turtle hit a bound
+  '''
+  right_bound = wn_width/2
+  left_bound = -right_bound
+  top_bound = wn_height/2
+  bottom_bound = -top_bound
+  vertical = ""
+  horizontal = ""
+  
+  if myturtle.xcor() <= left_bound:
+    horizontal = "left"
+  elif myturtle.xcor() >= right_bound:
+    horizontal = "right"
+  
+  if myturtle.ycor() <= bottom_bound:
+    vertical = "bottom"
+  elif myturtle.ycor() >= top_bound:
+    vertical = "top"
+  
+  if vertical == "" and horizontal == "":
+    return False
+  else:
+    return f"hit_{vertical}{horizontal}_bound"
+
+
+def screenSaver(duration = 30, myturtle = None, wn_height = 20, wn_width = 20):
+  '''
+  Bounces a turtle around a set area for a specified duration, and traces
+  a line across the trajectory.
+
+  args:
+    duration: (int) time the function will run
+    myturtle: (turtle.Turtle) a turtle object to be bounced
+    wn_height: (int) height of bound area
+    wn_width: (int) width of bound area
+  '''
+  # Keeps track of time passed by comparing the current
+  # time to the time the function is meant to end
+  time_start = int(time.time())
+  time_end = time_start + duration
+  time_now = int(time.time())
+  # Makes sure the length of one step does not exceed the
+  # width of the window
+  step_length = wn_width/100
+  # Move to origin and spin to face random direction
+  myturtle.penup()
+  myturtle.goto(0,0)
+  myturtle.pendown()
+  myturtle.setheading(random.randrange(-180, 181))
+  # Determines the angle of the turtle's trajectory 
+  # through its last position, then reflects the trajectory based on
+  # which boundary it hit. If the turtle hits a corner,
+  # the trajectory isn't reflected, but flipped 180.
+  while time_end > time_now:
+    last_pos = myturtle.pos()
+    myturtle.forward(step_length)
+    angle = myturtle.towards(last_pos) + 180
+    bound_hit = hitBounds(myturtle, wn_height, wn_width)
+    if bound_hit == "hit_topleft_bound" or bound_hit == "hit_topright_bound" or bound_hit == "hit_bottomleft_bound" or bound_hit == "hit_bottomright_bound":
+      myturtle.setheading(angle + 180)
+    elif bound_hit == "hit_left_bound" or bound_hit == "hit_right_bound":
+      myturtle.setheading(-angle + 180)
+    elif bound_hit == "hit_top_bound" or bound_hit == "hit_bottom_bound":
+      myturtle.setheading(-angle)
+    time_now = int(time.time())
+  
+
 class Dartboard:
   def __init__(self, circle_radius=1):
+    '''
+    Initialize the Dartboard object.
+    
+    args:
+    circle_radius: (int) radius of dartboard
+    '''
     self.myturtle = turtle.Turtle()
     self.myturtle.speed(0)
     self.window = turtle.Screen()
     self.circle_radius = circle_radius
     
-  def setCircle(self, circle_radius=1):  # Allows the user to set a new circle radius
+  
+  def setCircle(self, circle_radius=1):
+    '''
+    Set a new circle radius.
+    '''
     self.circle_radius = circle_radius
     
+  
   def setUpDartboard(self):
-    origin = 0
-    urxy = self.circle_radius            # Because the dartboard is a square, the top right and bottom left
-    llxy  = -self.circle_radius          # points will have x and y points of equal values (e.g. (1, 1))
-    square_width = self.circle_radius*2
-    
+    '''
+    Sets up a window for use by the Dartboard.
+    '''
+    originxy = 0                          # Because the dartboard is a square,
+    urxy = self.circle_radius             # the upper right and lower left
+    llxy  = -self.circle_radius           # points will have x and y points 
+    square_width = self.circle_radius*2   # of equal values (e.g. (1, 1))
+  
     self.window.setworldcoordinates(llxy, llxy, urxy, urxy)
     drawSquare(self.myturtle, square_width, top_left_x=llxy, top_left_y=urxy)
-    drawLine(self.myturtle, x_start=llxy, y_start=origin, x_end=urxy, y_end=origin)
-    drawLine(self.myturtle, x_start=origin, y_start=urxy, x_end=origin, y_end=llxy)
-    self.myturtle.goto(x=origin,y=llxy)                           # Moves turtle to (0, -1) since the 
-    drawCircle(self.myturtle, radius=self.circle_radius)          # circle is drawn from the bottom
+    drawAxes(self.myturtle, axis_width=square_width, origin_x=originxy, origin_y=originxy)
+    self.myturtle.goto(x=originxy,y=llxy)                           
+    drawCircle(self.myturtle, radius=self.circle_radius)
 
+  
+  def clearDartboard(self):
+    '''
+    Clear the Dartboard window.
+    '''
+    self.myturtle.clear()
+    
+ 
   def isInCircle(self):
-    return self.myturtle.distance(0, 0) <= self.circle_radius    # Assumes the circle is centered at (0, 0)
+    '''
+    Checks if the turtle used by the Dartboard is within the
+    dartboard circle.
 
+    return:
+      return: (bool) True if in circle, False if not
+    '''
+    return self.myturtle.distance(0, 0) <= self.circle_radius
+
+  
   def throwDart(self):
-    dot_x = random.uniform(-1, 1)
-    dot_y = random.uniform(-1, 1)
+    '''
+    Throws a dart that is blue if it hits the dartboard, and red if it misses
+    '''
+    dot_x = random.uniform(-self.circle_radius, self.circle_radius)
+    dot_y = random.uniform(-self.circle_radius, self.circle_radius)
     self.myturtle.penup()
     self.myturtle.goto(dot_x, dot_y)
     if self.isInCircle():
@@ -69,7 +246,49 @@ class Dartboard:
       self.myturtle.stamp()
     self.myturtle.color("black")
 
-  def playDarts(self, rounds = 10):
+  
+  def dartGuess(self, guess = "hit"):      # Throws dart and lets user guess
+    '''
+    Throws a dart, and checks if its hit/miss is the same as the guess.
+
+    args:
+      guess: (string) "hit" or "miss"
+
+    return: 
+      return: (bool) True if guess is same as dart, False if not
+    '''
+    self.throwDart()                       # if the dart hit
+    if guess == "hit":
+      return self.isInCircle() == True
+    if guess == "miss":
+      return self.isInCircle() == False
+
+  
+  def dartRandom(self, throws = 10):
+    '''
+    Throws a number of darts, and says how many hit.
+    
+    args:
+      throws: (int) number of thrown darts  
+
+    return:
+      hit: (int) number of darts hitting the board
+    '''
+    hit = 0
+    for throws in range(throws):
+      self.throwDart()
+      if self.isInCircle() == True:
+        hit += 1
+    return hit
+    
+  
+  def playDarts(self, rounds=10):
+    '''
+    Simulates a dart game between two players, and announces the winner.
+
+    args:
+      rounds: (int) number of rounds played
+    '''
     player_one_points = 0
     player_two_points = 0
     for i in range(rounds):
@@ -88,7 +307,17 @@ class Dartboard:
     else:
       print("It was a tie!")
 
+  
   def montePi(self, num_darts=0): 
+    '''
+    Performs a monte pi simulation to approximate pi.
+
+    args:
+    num_darts: (int) number of darts thrown in the simulation
+
+    return:
+      approx_pi: (float) calculated approximation of pi
+    '''
     inside_count = 0
     for i in range(num_darts):
       self.throwDart()
@@ -96,70 +325,88 @@ class Dartboard:
         inside_count += 1
     approx_pi = (inside_count / num_darts) * 4
     return approx_pi
-    
-def drawSquare(myturtle=None, width=0, top_left_x=0, top_left_y=0):
-  myturtle.penup()
-  myturtle.goto(top_left_x, top_left_y)
-  myturtle.pendown()
-  myturtle.goto(top_left_x+width, top_left_y)
-  myturtle.goto(top_left_x+width, top_left_y-width)
-  myturtle.goto(top_left_x, top_left_y-width)
-  myturtle.goto(top_left_x, top_left_y)
-  myturtle.penup()
   
-def drawLine(myturtle=None, x_start=0, y_start=0, x_end=0, y_end=0):
-  myturtle.penup()
-  myturtle.goto(x_start, y_start)
-  myturtle.pendown()
-  myturtle.goto(x_end, y_end)
-  myturtle.penup()
-  
-def drawCircle(myturtle=None, radius=0, resolution = 100):
-  myturtle.pendown()
-  myturtle.circle(radius, steps=resolution)
-  myturtle.penup()
+############################################################################
+### The first half of main() is just the original code rewritten         ###
+### to work with the Dartboard class. The second half is original code,  ###
+### written to demonstrate new features (e.g. the Dartboard class)       ###
+############################################################################
 
-#########################################################
-#         Do not alter any code below here              #
-#       Your code must work with the main proivided     #
-#########################################################
 def main():
-    # Get number of darts for simulation from user
-    # Note continuation character <\> so we don't go over 78 columns:
-    print("This is a program that simulates throwing darts at a dartboard\n" \
-        "in order to approximate pi: The ratio of darts in a unit circle\n"\
-        "to the total number of darts in a 2X2 square should be\n"\
-        "approximately  equal to pi/4")
-    print("=========== Part A ===========")
+  # Get number of darts for simulation from user
+  # Note continuation character <\> so we don't go over 78 columns:
+  print("This is a program that simulates throwing darts at a dartboard\n" \
+      "in order to approximate pi: The ratio of darts in a unit circle\n"\
+      "to the total number of darts in a 2X2 square should be\n"\
+      "approximately  equal to pi/4")
+  print("=========== Part A ===========")
+  
+  # Creates a Dartboard object and sets it up
+  dartboard = Dartboard()
+  dartboard.setUpDartboard()
+  
+  # Loop for 10 darts to test your code
+  for i in range(10):
+      dartboard.throwDart()
+  print("\tPart A Complete...")
+  print("=========== Part B ===========")
+  dartboard.clearDartboard()
+  dartboard.setUpDartboard()
+  dartboard.playDarts()
+  print("\tPart B Complete...")
+  # Keep the window up until dismissed
+  print("=========== Part C ===========")
+  dartboard.clearDartboard()
+  dartboard.setUpDartboard()
+  
+  # Includes the following code in order to update animation periodically
+  # instead of for each throw (saves LOTS of time):
+  BATCH_OF_DARTS = 5000
+  dartboard.window.tracer(BATCH_OF_DARTS)
+  
+  # Conduct simulation and print result
+  number_darts = int(input("\nPlease input the number of darts to be thrown in the simulation:  "))
+  approx_pi = dartboard.montePi(number_darts)
+  print("\nThe estimation of pi using "+str(number_darts)+" virtual darts is " + str(approx_pi))
+  print("\tPart C Complete...")
+  # Don't hide or mess with window while it's 'working'
 
-    # Creates a Dartboard object and sets it up
-    dartboard = Dartboard()
+  #############################################
+  ### Everything past this point is new code ##
+  #############################################
+  print("\n========== Midterm  ==========")      
+  dartboard.window.tracer(1)
+  dartboard.clearDartboard()               # This shows that the dartboard
+  dartboard.setCircle(10)                  # functions the same even with
+  print("Redrawing dartboard with new radius 10...") # a new radius
+  dartboard.setUpDartboard()
+  print("Replaying dart game...")
+  dartboard.playDarts()
+  print("\nThrowing random darts...")
+  randomThrows = int(input("How many darts would you like to throw? "))
+  randomHit = dartboard.dartRandom(randomThrows)
+  print(f"{randomHit} darts hit!")
 
-    # Loop for 10 darts to test your code
-    for i in range(10):
-        dartboard.throwDart()
-    print("\tPart A Complete...")
-    print("=========== Part B ===========")
-    dartboard.myturtle.clear()
-    dartboard.setUpDartboard()
-    dartboard.playDarts()
-    print("\tPart B Complete...")
-    # Keep the window up until dismissed
-    print("=========== Part C ===========")
-    dartboard.myturtle.clear()
-    dartboard.setUpDartboard()
-    
-    # Includes the following code in order to update animation periodically
-    # instead of for each throw (saves LOTS of time):
-    BATCH_OF_DARTS = 5000
-    dartboard.window.tracer(BATCH_OF_DARTS)
-
-    # Conduct simulation and print result
-    number_darts = int(input("\nPlease input the number of darts to be thrown in the simulation:  "))
-    approx_pi = dartboard.montePi(number_darts)
-    print("\nThe estimation of pi using "+str(number_darts)+" virtual darts is " + str(approx_pi))
-    print("\tPart C Complete...")
-    # Don't hide or mess with window while it's 'working'
-    dartboard.window.exitonclick()
-
+  print("\nPlaying guessing game...")
+  myguess = str(input("Hit or miss?\n"))
+  myguess = myguess.lower()
+  if dartboard.dartGuess(myguess) == True:
+    if myguess == "hit":
+      print("\nCorrect! The dart hit.")
+    else:
+      print("\nCorrect! The dart missed.")
+  else:
+    if myguess == "hit":
+      print("\nIncorrect! The dart missed.")
+    else:
+      print("\nIncorrect! The dart hit.")
+  
+  screensaver_time = int(input("How many seconds would you like to play the screensaver for? "))
+  print(f"\nBeginning Screensaver for {screensaver_time} seconds...")
+  dartboard.clearDartboard()
+  drawSquare(dartboard.myturtle, width=dartboard.circle_radius*2, top_left_x=-dartboard.circle_radius, top_left_y=dartboard.circle_radius)
+  screenSaver(duration=screensaver_time, myturtle=dartboard.myturtle, wn_height=dartboard.circle_radius*2, wn_width = dartboard.circle_radius*2)
+  print("Screensaver has completed...")
+  print("\nThe program demo has concluded. \nThank you for your time!")
+  dartboard.window.exitonclick()
 main()
